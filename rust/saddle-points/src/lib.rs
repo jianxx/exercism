8fd30d9a8203936a037f04e2d91b3650877adeda
Xiaoxiang JIAN <xiaoxiang.jxx@alibaba-inc.com>
2020-09-02 16:24:22 +0800

    Resolve saddle-points

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -1,6 +1,11 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    unimplemented!(
-        "find the saddle points of the following matrix: {:?}",
-        input
-    )
+    let mut r = vec!();
+    for (row_num, row) in input.iter().enumerate() {
+        for (col_num, v) in row.iter().enumerate() {
+            if row.iter().all(|x| x<=v) && input.iter().all(|x| *v<=x[col_num]) {
+                r.push((row_num, col_num));
+            }
+        }
+    }
+    return r;
 }
