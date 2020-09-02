224917954147020273b03469a41a9209155a6859
Xiaoxiang JIAN <xiaoxiang.jxx@alibaba-inc.com>
2020-09-02 14:41:11 +0800

    Resolve grains

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,7 +1,10 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    if s<1 || s>64 {
+        panic!("Square must be between 1 and 64");
+    }
+    u64::pow(2,s-1)
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    (1..65).map(|x| square(x)).sum()
 }
