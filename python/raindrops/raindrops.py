98c100b38f25c76eab10314f0fd23a153bb87220
Xiaoxiang JIAN <xiaoxiang.jxx@alibaba-inc.com>
2020-09-02 17:53:46 +0800

    Resolve raindrops and leap

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,13 @@
 def convert(number):
-    pass
+    if(number%3!=0 and number%5!=0 and number%7!=0):
+        return str(number)
+    else:
+        r = ''
+        if number%3==0:
+            r += 'Pling'
+        if number%5==0:
+            r += 'Plang'
+        if number%7==0:
+            r += 'Plong'
+
+        return r
