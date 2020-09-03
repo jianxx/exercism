cda42d87c61163444fcfbd7c55025e353d52d73f
Xiaoxiang JIAN <xiaoxiang.jxx@alibaba-inc.com>
2020-09-03 11:29:49 +0800

    Resolve rational-numbers

--- python/rational-numbers/rational_numbers.py
+++ python/rational-numbers/rational_numbers.py
@@ -3,8 +3,9 @@ from __future__ import division
 
 class Rational:
     def __init__(self, numer, denom):
-        self.numer = None
-        self.denom = None
+        self.numer = numer
+        self.denom = denom
+        self.__normalize__()
 
     def __eq__(self, other):
         return self.numer == other.numer and self.denom == other.denom
@@ -13,22 +14,50 @@ class Rational:
         return '{}/{}'.format(self.numer, self.denom)
 
     def __add__(self, other):
-        pass
+        return Rational(self.numer * other.denom + self.denom * other.numer, self.denom * other.denom)
 
     def __sub__(self, other):
-        pass
+        return Rational(self.numer * other.denom - self.denom * other.numer, self.denom * other.denom)
 
     def __mul__(self, other):
-        pass
+        return Rational(self.numer * other.numer, self.denom * other.denom)
 
     def __truediv__(self, other):
-        pass
+        return Rational(self.numer * other.denom, self.denom * other.numer)
 
     def __abs__(self):
-        pass
+        return Rational(abs(self.numer), abs(self.denom))
 
     def __pow__(self, power):
-        pass
+        if str(power).isdigit():
+            return Rational(pow(self.numer, abs(power)), pow(self.denom, abs(power)))
+        else:
+            return Rational(pow(self.numer, power), pow(self.denom, power))
 
     def __rpow__(self, base):
-        pass
+        return pow(pow(base, self.numer), 1/self.denom)
+
+    def __normalize__(self):
+        if self.numer==0:
+            self.denom = 1
+            pass
+
+        if abs(self.numer) > abs(self.denom):
+            smaller = abs(self.denom)
+        else:
+            smaller = abs(self.numer)
+        
+        hcf = 1
+        for i in range(1,int(smaller) + 1):
+            if((self.numer % i == 0) and (self.denom % i == 0)):
+                hcf = i
+        
+        self.numer /= hcf
+        self.denom /= hcf
+
+        if self.numer<0 and self.denom<0:
+            self.numer = abs(self.numer)
+            self.denom = abs(self.denom)
+        elif self.numer>0 and self.denom<0:
+            self.numer *= -1
+            self.denom *= -1
