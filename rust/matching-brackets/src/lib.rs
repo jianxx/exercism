ffaa5acda2afad2d705a9993ed547d4676ca0fdc
Xiaoxiang JIAN <xiaoxiang.jxx@alibaba-inc.com>
2020-09-03 19:26:22 +0800

    Resolve matching-brackets

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,6 +1,26 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    let mut l: Vec<char> = Vec::new();
+    for c in string.chars() {
+        match c {
+            '{' | '[' | '(' => l.push(c),
+            '}' => {
+                if l.pop() != Some('{') {
+                    return false;
+                }
+            }
+            ']' => {
+                if l.pop() != Some('[') {
+                    return false;
+                }
+            }
+            ')' => {
+                if l.pop() != Some('(') {
+                    return false;
+                }
+            }
+            _ => {}
+        }
+    }
+
+    l.is_empty()
 }
