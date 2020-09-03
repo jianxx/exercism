74d7e1ea4013e2f0f956d3ac23bf277e45412f8e
Xiaoxiang JIAN <xiaoxiang.jxx@alibaba-inc.com>
2020-09-03 15:49:25 +0800

    Resolve bank-account

--- python/bank-account/bank_account.py
+++ python/bank-account/bank_account.py
@@ -1,18 +1,45 @@
+import threading
+
 class BankAccount:
     def __init__(self):
-        pass
+        self.opened = False
+        self.balance = 0
+        self.lock = threading.Lock()
 
     def get_balance(self):
-        pass
+        if not self.opened:
+            raise ValueError("Account is not open")
+        return self.balance
 
     def open(self):
-        pass
+        if self.opened:
+            raise ValueError("Do not open an existing account")
+        self.opened = True
 
     def deposit(self, amount):
-        pass
+        if not self.opened or amount < 0:
+            raise ValueError("Account is not open")
+        else:
+            self.__add_to_balance(amount)
 
     def withdraw(self, amount):
-        pass
+        if not self.opened or amount > self.balance or amount < 0:
+            raise ValueError("Account is not open")
+        else:
+            self.__add_to_balance(0-amount)
 
     def close(self):
-        pass
+        if not self.opened:
+            raise ValueError("Account is not open")
+        self.opened = False
+        self.balance = 0
+
+    def __add_to_balance(self, amount):
+        self.lock.acquire()
+        newbalance = self.balance + amount
+        if newbalance >= 0:
+            self.balance = newbalance
+            self.lock.release()
+        else:
+            self.lock.release()
+            raise ValueError("New balance < 0")
