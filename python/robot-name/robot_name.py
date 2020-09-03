42ed15eb8e016b127035cd52a4ffe18bc4c54bf3
Xiaoxiang JIAN <xiaoxiang.jxx@alibaba-inc.com>
2020-09-03 16:45:07 +0800

    Resolve robot-name

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,3 +1,26 @@
+import random
+from string import ascii_uppercase
+
+ALPHABET_SIZE = len(ascii_uppercase)
+
 class Robot:
+    name_used = set()
+
     def __init__(self):
-        pass
+        self.reset()
+
+    def reset(self):
+        name = self.__random_name()
+        while name in Robot.name_used:
+            name = self.__random_name()
+        Robot.name_used.add(name)
+        self.name = name
+
+    def __random_name(self):
+        return self.__random_letters(2) + self.__random_digits(3)
+
+    def __random_letters(self, n):
+        return ''.join([ ascii_uppercase[random.randint(0,ALPHABET_SIZE-1)] for _ in range(n) ])
+
+    def __random_digits(self, n):
+        return ''.join([ str(random.randint(0,9)) for _ in range(n) ])
