b854e6c49abacb2ac4970bdb948e45a1f984ae75
Xiaoxiang JIAN <xiaoxiang.jxx@alibaba-inc.com>
2020-09-07 18:22:50 +0800

    Resolve bob

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+    let m = message.trim();
+    if m.is_empty() {
+        return "Fine. Be that way!";
+    }
+    let is_question = m.ends_with('?');
+    let is_yell = m.contains(char::is_alphabetic) && !m.contains(char::is_lowercase);
+    match(is_question, is_yell) {
+        (true, true) => "Calm down, I know what I'm doing!",
+        (true, false) => "Sure.",
+        (false, true) => "Whoa, chill out!",
+        (false, false) => "Whatever.",
+    }
 }
