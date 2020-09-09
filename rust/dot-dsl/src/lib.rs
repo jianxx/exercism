02856df2bcefef1ef4bcbbc04cdb405f256472a4
Xiaoxiang JIAN <xiaoxiang.jxx@alibaba-inc.com>
2020-09-09 11:28:05 +0800

    Resolve dot-dsl

--- rust/dot-dsl/src/lib.rs
+++ rust/dot-dsl/src/lib.rs
@@ -1,9 +1,103 @@
+
 pub mod graph {
-    pub struct Graph;
+    use std::collections::HashMap;
+    use graph_items::node::Node;
+    use graph_items::edge::Edge;
+
+    pub struct Graph {
+        pub nodes: Vec<Node>,
+        pub edges: Vec<Edge>,
+        pub attrs: HashMap<String, String>
+    }
 
     impl Graph {
         pub fn new() -> Self {
-            unimplemented!("Construct a new Graph struct.");
+            Graph {
+                nodes: Vec::new(),
+                edges: Vec::new(),
+                attrs: HashMap::new()
+            }
+        }
+
+        pub fn with_nodes(mut self, nodes: &Vec<graph_items::node::Node>) -> Self {
+            self.nodes = nodes.clone().to_vec();
+            self
+        }
+
+        pub fn with_edges(mut self, edges: &Vec<graph_items::edge::Edge>) -> Self {
+            self.edges = edges.clone().to_vec();
+            self
+        }
+
+        pub fn with_attrs(mut self, attrs: &[(&'static str, &'static str)]) -> Self {
+            attrs.iter().for_each(|(key, value)| {
+                self.attrs.insert(key.to_string(), value.to_string());
+            });
+            self
+        }
+
+        pub fn get_node(&self, name: &str) -> Option<&Node> {
+            self.nodes.iter().filter(|x| x.name==name).next()
+        }
+    }
+
+    pub mod graph_items {
+        pub mod edge {
+            use std::collections::HashMap;
+
+            #[derive(Clone, Debug, PartialEq)]
+            pub struct Edge {
+                src: String,
+                target: String,
+                attrs: HashMap<String, String>,
+            }
+
+            impl Edge {
+                pub fn new(src: &str, target: &str) -> Self {
+                    Edge {
+                        src: src.to_string(),
+                        target: target.to_string(),
+                        attrs: HashMap::new()
+                    }
+                }
+
+                pub fn with_attrs(mut self, attrs: &[(&'static str, &'static str)]) -> Self {
+                    attrs.iter().for_each(|(key, value)| {
+                        self.attrs.insert(key.to_string(), value.to_string());
+                    });
+                    self
+                }
+            }
+        }
+
+        pub mod node {
+            use std::collections::HashMap;
+
+            #[derive(Clone, Debug, PartialEq)]
+            pub struct Node {
+                pub name: String,
+                attrs: HashMap<String, String>,
+            }
+
+            impl Node {
+                pub fn new(name: &str) -> Self {
+                    Node {
+                        name: name.to_string(),
+                        attrs: HashMap::new()
+                    }
+                }
+
+                pub fn with_attrs(mut self, attrs: &[(&'static str, &'static str)]) -> Self {
+                    attrs.iter().for_each(|(key, value)| {
+                        self.attrs.insert(key.to_string(), value.to_string());
+                    });
+                    self
+                }
+
+                pub fn get_attr(&self, name: &str) -> Option<&str> {
+                    self.attrs.get(name).map(|v| v.as_ref())
+                }
+            }
         }
     }
 }
