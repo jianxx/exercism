c25114d6575fddf66c50cd561b81b633e88f09d8
Xiaoxiang JIAN <xiaoxiang.jxx@alibaba-inc.com>
2020-09-12 22:37:46 +0800

    Refactor raindrops

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,12 +1,12 @@
 def convert(number):
-    r = ''
-    if number%3==0:
-        r += 'Pling'
-    if number%5==0:
-        r += 'Plang'
-    if number%7==0:
-        r += 'Plong'
-    if r == '':
+    r = ""
+    if number % 3 == 0:
+        r += "Pling"
+    if number % 5 == 0:
+        r += "Plang"
+    if number % 7 == 0:
+        r += "Plong"
+    if r == "":
         return str(number)
     else:
         return r
