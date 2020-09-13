62dea7434465f43e7729efba50c08ed8bd2c079b
Xiaoxiang JIAN <xiaoxiang.jxx@alibaba-inc.com>
2020-09-13 23:30:28 +0800

    Resolve anagram

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,26 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+    let word = word.to_lowercase();
+    let anagrams = word.chars().fold(HashMap::new(), |mut anagrams, c| {
+        *anagrams.entry(c).or_insert(0) += 1;
+        anagrams
+    });
+
+    possible_anagrams
+        .iter()
+        .filter(|anagram| {
+            if anagram.len() != word.len() {
+                return false;
+            }
+            let anagram = anagram.to_lowercase();
+            let charmap = anagram.chars().fold(HashMap::new(), |mut anagrams, c| {
+                *anagrams.entry(c).or_insert(0) += 1;
+                anagrams
+            });
+            anagram != word && charmap == anagrams
+        })
+        .map(|x| *x)
+        .collect::<HashSet<&str>>()
 }
