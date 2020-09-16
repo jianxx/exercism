a117fe28d387afc1d4c4912cd420b8b26261d97a
Xiaoxiang JIAN <xiaoxiang.jxx@alibaba-inc.com>
2020-09-16 10:14:05 +0800

    Refactor hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,15 +1,14 @@
 package hamming
 
-import (
-	"fmt"
-)
+import "errors"
 
 // Distance returns the Hamming Distance between two strings.
 func Distance(a, b string) (int, error) {
-	diff := 0
 	if len(a) != len(b) {
-		return 0, fmt.Errorf("a and b are difference size")
+		return 0, errors.New("a and b are difference size")
 	}
+
+	diff := 0
 	for i := 0; i < len(a); i++ {
 		if a[i] != b[i] {
 			diff++
