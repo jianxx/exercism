2b5bcbed6c3a54bd3d1b2f6618e0c29f66061131
Xiaoxiang JIAN <xiaoxiang.jxx@alibaba-inc.com>
2020-09-20 19:00:21 +0800

    Resolve accumulate

--- go/accumulate/accumulate.go
+++ go/accumulate/accumulate.go
@@ -1,1 +1,10 @@
 package accumulate
+
+// Accumulate will perform an operation on each element of a given collection
+func Accumulate(input []string, operation func(string) string) []string {
+	var r = []string{}
+	for _, e := range input {
+		r = append(r, operation(e))
+	}
+	return r
+}
