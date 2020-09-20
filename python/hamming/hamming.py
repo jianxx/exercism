e4fb39a04f59187d113459ab242fdd9038d1a015
Xiaoxiang JIAN <xiaoxiang.jxx@alibaba-inc.com>
2020-09-20 23:23:39 +0800

    Resolve hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,8 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("distance(%s, %s)" % (strand_a, strand_b))
+    diff = 0
+    for i in range(len(strand_a)):
+        if strand_a[i] != strand_b[i]:
+            diff = diff + 1
+    return diff
