171bf9d7996964068942f88f523fafcfedd2c6d3
Xiaoxiang JIAN <xiaoxiang.jxx@alibaba-inc.com>
2020-09-20 14:41:04 +0800

    Add all to practice branch

--- rust/simple-linked-list/src/lib.rs
+++ rust/simple-linked-list/src/lib.rs
@@ -1,34 +1,72 @@
 use std::iter::FromIterator;
 
+#[derive(Debug)]
+struct Node<T> {
+    pub data: T,
+    pub next: Option<Box<Node<T>>>,
+}
+
+impl<T> Node<T> {
+    pub fn new(e: T, next: Option<Box<Node<T>>>) -> Self {
+        Node {
+            data: e,
+            next: next,
+        }
+    }
+}
+
 pub struct SimpleLinkedList<T> {
     // Delete this field
     // dummy is needed to avoid unused parameter error during compilation
-    dummy: ::std::marker::PhantomData<T>,
+    // dummy: ::std::marker::PhantomData<T>,
+    head: Option<Box<Node<T>>>,
+    length: usize,
 }
 
 impl<T> SimpleLinkedList<T> {
     pub fn new() -> Self {
-        unimplemented!()
+        SimpleLinkedList {
+            head: None,
+            length: 0,
+        }
     }
 
     pub fn len(&self) -> usize {
-        unimplemented!()
+        self.length
     }
 
     pub fn push(&mut self, _element: T) {
-        unimplemented!()
+        let node = Box::new(Node::new(_element, self.head.take()));
+        self.head = Some(node);
+        self.length += 1;
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        unimplemented!()
+        match self.length {
+            0 => None,
+            _ => {
+                self.length -= 1;
+                self.head.take().map(|x| {
+                    let node = *x;
+                    self.head = node.next;
+                    node.data
+                })
+            }
+        }
     }
 
     pub fn peek(&self) -> Option<&T> {
-        unimplemented!()
+        self.head.as_ref().map(|x| &x.data)
     }
+}
 
+impl<T: Clone> SimpleLinkedList<T> {
     pub fn rev(self) -> SimpleLinkedList<T> {
-        unimplemented!()
+        let mut rev_list: SimpleLinkedList<T> = SimpleLinkedList::new();
+        self.head.as_ref().map(|node| {
+            rev_list.push(node.data.clone());
+        });
+        rev_list
     }
 }
 
