fed294acbfcacc7f341a890b1be5491e0bd8df60
Xiaoxiang JIAN <xiaoxiang.jxx@alibaba-inc.com>
2020-09-20 14:41:04 +0800

    Add all to practice branch

--- go/accumulate/accumulate.go
+++ go/accumulate/accumulate.go
@@ -1,4 +1,5 @@
 package accumulate
+<<<<<<< HEAD
 
 // Accumulate will perform an operation on each element of a given collection
 func Accumulate(input []string, operation func(string) string) []string {
@@ -8,3 +9,5 @@ func Accumulate(input []string, operation func(string) string) []string {
 	}
 	return r
 }
+=======
+>>>>>>> f81b578... Add all to practice branch
