2b5e1c7033c6f1dd63ac688d4dce00973da5376c
Xiaoxiang JIAN <jianxx@gmail.com>
2020-09-22 18:57:18 +0800

    Resolve protein-translation

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,27 @@
 def proteins(strand):
-    pass
+    translater = {
+        "AUG": "Methionine",
+        "UUU": "Phenylalanine",
+        "UUC": "Phenylalanine",
+        "UUA": "Leucine",
+        "UUG": "Leucine",
+        "UCU": "Serine",
+        "UCC": "Serine",
+        "UCA": "Serine",
+        "UCG": "Serine",
+        "UAU": "Tyrosine",
+        "UAC": "Tyrosine",
+        "UGU": "Cysteine",
+        "UGC": "Cysteine",
+        "UGG": "Tryptophan",
+        "UAA": "STOP",
+        "UAG": "STOP",
+        "UGA": "STOP",
+    }
+    proteins = []
+    for codon in [strand[i : i + 3] for i in range(0, len(strand), 3)]:
+        if translater.get(codon, None):
+            if translater.get(codon, None) == "STOP":
+                break
+            proteins.append(translater.get(codon, None))
+    return proteins
