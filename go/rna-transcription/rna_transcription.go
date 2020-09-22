046c573e5d8cb6d484bb429974a3df9dcf4e6727
Xiaoxiang JIAN <xiaoxiang.jxx@alibaba-inc.com>
2020-09-22 10:33:23 +0800

    Resolve rna-transcription

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,4 +1,21 @@
 package strand
 
+import "bytes"
+
+// ToRNA return its RNA complement for the input dna
 func ToRNA(dna string) string {
+	var b bytes.Buffer
+	for _, c := range dna {
+		switch c {
+		case 'G':
+			b.WriteString("C")
+		case 'C':
+			b.WriteString("G")
+		case 'T':
+			b.WriteString("A")
+		case 'A':
+			b.WriteString("U")
+		}
+	}
+	return b.String()
 }
