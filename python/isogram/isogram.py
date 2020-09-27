e2bce1911c40a5397abcbefadd55faaa1b874174
Xiaoxiang JIAN <jianxx@gmail.com>
2020-09-27 10:02:37 +0800

    Resolve isogram

---
+++ python/isogram/isogram.py
@@ -0,0 +1,12 @@
+from string import ascii_lowercase
+
+
+def is_isogram(string):
+    charset = set()
+    for c in string.lower():
+        if c in ascii_lowercase:
+            if c in charset:
+                return False
+            else:
+                charset.add(c)
+    return True
