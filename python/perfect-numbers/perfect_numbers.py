45f8d4e955098e846cd776a78b6ad2bd996c7667
Xiaoxiang JIAN <jianxx@gmail.com>
2020-09-28 22:21:00 +0800

    Refactor perfect-numbers

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,10 +1,7 @@
 def classify(number):
     if number <= 0:
         raise ValueError("invalid number: %s", number)
-    aliquot_sum = 0
-    for x in range(1, number):
-        if number % x == 0:
-            aliquot_sum += x
+    aliquot_sum = sum([x for x in range(1, number) if number % x == 0])
     if aliquot_sum == number:
         return "perfect"
     elif aliquot_sum > number:
