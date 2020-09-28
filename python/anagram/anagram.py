80d50ef8e9cd667eab75ed7fc8052329344735a6
Xiaoxiang JIAN <jianxx@gmail.com>
2020-09-28 09:51:59 +0800

    Refector anagram with collections.Counter

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,26 +1,12 @@
-import itertools
-from string import ascii_lowercase
+from collections import Counter
 
 
 def find_anagrams(word, candidates):
-    charcount = [0] * 26
+    word_counter = Counter(word.lower())
 
-    for c in word.lower():
-        if c in ascii_lowercase:
-            charcount[ord(c) - ord("a")] += 1
-
-    def is_anagram(candidate):
-        if len(word) != len(candidate):
-            return False
-        candidate_count = [0] * 26
-        for c in candidate.lower():
-            if c in ascii_lowercase:
-                candidate_count[ord(c) - ord("a")] += 1
-        return word.lower() != candidate.lower() and all(
-            charcount[i] == candidate_count[i] for i in range(len(charcount))
-        )
-
-    return itertools.filterfalse(
-        lambda candidate: not is_anagram(candidate), candidates
-    )
-
+    return [
+        candidate
+        for candidate in candidates
+        if word.lower() != candidate.lower()
+        and Counter(candidate.lower()) == word_counter
+    ]
