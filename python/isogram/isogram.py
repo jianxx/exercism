eaded210382bd75c93a74030365641a69567f6b9
Xiaoxiang JIAN <jianxx@gmail.com>
2020-10-08 00:18:44 +0800

    Refactor isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,13 +1,11 @@
-import itertools
 from string import ascii_lowercase
 
 
 def is_isogram(string):
-    charcount = [0] * 26
-
-    def addchar(c):
+    charset = set()
+    for c in string.lower():
         if c in ascii_lowercase:
-            charcount[ord(c) - ord("a")] += 1
-
-    itertools.starmap(lambda c: addchar(c), string.lower())
-    return all(count < 2 for count in charcount)
+            if c in charset:
+                return False
+            charset.add(c)
+    return True
