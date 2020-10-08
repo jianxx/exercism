8f84c76265ee6220dd4675d22a87078055fa1e30
Xiaoxiang JIAN <jianxx@gmail.com>
2020-10-08 15:44:12 +0800

    Refactor scrabble-score

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,15 +1,24 @@
 package scrabble
 
 import (
-	"strings"
+	"unicode"
 )
+
+// valueMap stores the value for each letter
+var valueMap = map[rune]int{'a': 1, 'e': 1, 'i': 1, 'l': 1, 'n': 1, 'o': 1, 'r': 1, 's': 1, 't': 1, 'u': 1,
+	'd': 2, 'g': 2,
+	'b': 3, 'c': 3, 'm': 3, 'p': 3,
+	'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4,
+	'k': 5,
+	'j': 8, 'x': 8,
+	'q': 10, 'z': 10}
 
 // Score : given a word, compute the Scrabble score for that word.
 func Score(input string) int {
-	var valueMap = map[rune]int{'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
 	var score = 0
-	for _, c := range strings.ToLower(input) {
-		score += valueMap[c]
+	// Sum the values of each letter in the input word
+	for _, c := range input {
+		score += valueMap[unicode.ToLower(c)]
 	}
 	return score
 }
