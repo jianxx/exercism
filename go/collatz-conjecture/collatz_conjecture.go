6271ef882893cb7a639bc1eb3807d8810ad6f447
Xiaoxiang JIAN <jianxx@gmail.com>
2020-10-10 22:28:52 +0800

    Resolve collatz-conjecture

---
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -0,0 +1,21 @@
+package collatzconjecture
+
+import (
+	"fmt"
+)
+
+func CollatzConjecture(input int) (int, error) {
+	if input <= 0 {
+		return 0, fmt.Errorf("Invalid input: %d", input)
+	}
+	var steps = 0
+	for input != 1 {
+		if input%2 == 0 {
+			input /= 2
+		} else {
+			input = 3*input + 1
+		}
+		steps += 1
+	}
+	return steps, nil
+}
