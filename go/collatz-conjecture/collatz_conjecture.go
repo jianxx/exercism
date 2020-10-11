6f89515c848fecab9b7e8fea08c43d92855063f0
Xiaoxiang JIAN <jianxx@gmail.com>
2020-10-11 22:35:28 +0800

    Build go tracks with bazel

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,13 +1,15 @@
 package collatzconjecture
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func CollatzConjecture(input int) (int, error) {
 	if input <= 0 {
 		return 0, fmt.Errorf("Invalid input: %d", input)
 	}
+	if input == 1 {
+		return 0, nil
+	}
+
 	var steps = 0
 	for input != 1 {
 		if input%2 == 0 {
