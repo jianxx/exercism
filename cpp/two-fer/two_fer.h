c2bf5d591d0ff552a5a6728a95c8c4e9bed3425a
Xiaoxiang JIAN <jianxx@gmail.com>
2020-10-27 15:28:50 +0800

    Resolve two-fer

---
+++ cpp/two-fer/two_fer.h
@@ -0,0 +1,10 @@
+#if !defined(TWO_FER_H)
+#define TWO_FER_H
+#include <string>
+
+namespace two_fer {
+using namespace std;
+string two_fer(const string& name = "you");
+}  // namespace two_fer
+
+#endif  //TWO_FER_H
