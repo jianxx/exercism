808f7f0cbfd132de89deac9b914de4d39a15e20c
Xiaoxiang JIAN <jianxx@gmail.com>
2020-11-04 11:35:39 +0800

    Refactor luhn

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,24 +1,26 @@
 package luhn
 
+// Valid determines whether or not a number is valid per the Luhn formula.
 func Valid(number string) bool {
 	digitCount := 0
 	checkSum := 0
 	for i := len(number) - 1; i >= 0; i-- {
-		if number[i] == ' ' {
+		switch {
+		case number[i] == ' ':
 			continue
-		} else if number[i]-'0' > 9 {
+		case number[i] >= '0' && number[i] <= '9':
+			n := int(number[i] - '0') // iterator value
+			if digitCount%2 != 0 {
+				n <<= 1
+				if n > 9 {
+					n -= 9
+				}
+			}
+			checkSum += n
+			digitCount++
+		default:
 			return false
 		}
-		if digitCount%2 != 0 {
-			if (number[i]-'0')*2 > 9 {
-				checkSum += int(number[i]-'0')*2 - 9
-			} else {
-				checkSum += int(number[i]-'0') * 2
-			}
-		} else {
-			checkSum += int(number[i] - '0')
-		}
-		digitCount += 1
 	}
 	return digitCount > 1 && checkSum%10 == 0
 }
